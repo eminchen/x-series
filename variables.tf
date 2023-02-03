b23c8b5b5f59efdb86b42c238fb62d204006581e
eminchen <eminchen@cisco.com>
2023-02-02 19:24:32 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,13 +4,13 @@
 variable "api_key" {
     description = "API Key for Intersight Account"
     type = string
-    default = "59af0e11f11aa10001678016/59af0ceef11aa100016748f0/61f043507564612d33eda2d3"
+    #default = "59af0e11f11aa10001678016/59af0ceef11aa100016748f0/61f043507564612d33eda2d3"
 }
 
 variable "secretkey" {
     description = "Filename (PEM) that provides secret key for Intersight API"
     type = string
-    default = "SecretKey.txt"
+    #default = "SecretKey.txt"
 }
 
 variable "endpoint" {
@@ -21,6 +21,6 @@ variable "endpoint" {
 
 variable "organization" {
     type = string
-    default = "default"
+    default = "buffalo_dc"
 }
 
