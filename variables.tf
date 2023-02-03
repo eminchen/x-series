1ededa219e49ac36c99e459d70af9ee169f5e2b8
eminchen <eminchen@cisco.com>
2023-02-02 20:20:53 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 # https://intersight.com/an/settings/api-keys/
 ## Generate API key to obtain the API Key and PEM file
 
-variable "api_key" {
+variable "intersight_key" {
     description = "API Key for Intersight Account"
     type = string
     #default = "59af0e11f11aa10001678016/59af0ceef11aa100016748f0/61f043507564612d33eda2d3"
 }
 
-variable "secretkey" {
+variable "intersight_secret" {
     description = "Filename (PEM) that provides secret key for Intersight API"
     type = string
     #default = "SecretKey.txt"
